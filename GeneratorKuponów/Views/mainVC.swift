4222b5cfdb89ed1ac4dd821a90fa3f4e89c3f788
Marcin Miller <marcin.miller54@icloud.com>
2018-08-28 00:54:56 +0200

    dodano pliki projektu

---
+++ GeneratorKuponów/Views/mainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeneratorKuponów
+//
+//  Created by Marcin Miller on 27.08.2018.
+//  Copyright © 2018 Marcin Miller. All rights reserved.
+//
+
+import UIKit
+
+class mainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
